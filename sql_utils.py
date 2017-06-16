636872e4b1ee75759f91cad379bb1d4fec5bb9d3
jerryxhe <rebound1618@gmail.com>
2017-06-16 01:56:34 -0400

    CaseConversion class
    case conversions encapsulated in a class

--- sql_utils.py
+++ sql_utils.py
@@ -10,3 +10,16 @@ def camelcase_2_underscore(name):
     """PostgreSQL does not like upper case"""
     return "".join(c.isupper() and ("_"+c.lower()) or c
            for c in name)   
+
+class CaseConversion:
+    import re
+    def __init__(self):
+        self._pattern = re.compile(r'\_(?P<char>?)',re.I)
+    
+    def camelcase_2_underscore(self, name):
+        """PostgreSQL does not like upper case"""
+        return "".join(c.isupper() and ("_"+c.lower()) or c
+               for c in name)
+
+    def underscore_2_camelcase(self, name):
+        return self._pattern.sub(lambda mat:mat.groupdict()['char'].upper(),name)
