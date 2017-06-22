ed881da298c0a8dfb96c82457e98e62f08c064c1
jerryxhe <rebound1618@gmail.com>
2017-06-22 13:09:51 -0400

    Update jerrylibcommon.py
    batch iteration

--- jerrylibcommon.py
+++ jerrylibcommon.py
@@ -8,6 +8,16 @@ def first_of(iterator_, condition_):
         except Exception as e:
             pass
 
+def batch_iter(my_iter, num_per_batch=20, limit=None):
+    from itertools import islice
+    start_index =0
+    while start_index<limit:
+        try:
+            yield [row for row in islice(my_iter,num_per_batch)]
+            start_index += num_per_batch
+        except StopIteration as e:
+            return []
+
 def flatten_dict_iter(dic):
     for k,v in dic.iteritems():
         if isinstance(v, int):
