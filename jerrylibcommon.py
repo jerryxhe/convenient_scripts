862a326aca6f5f50d3447217349860a14d9dc1ff
jerryxhe <rebound1618@gmail.com>
2017-06-22 11:05:15 -0400

    Update jerrylibcommon.py
    do_shell is a fault tolerant system call function

--- jerrylibcommon.py
+++ jerrylibcommon.py
@@ -24,7 +24,6 @@ def flatten_dict_iter(dic):
 def condense_attributes(dic):
     return dict(flatten_dict_iter(dic))
 
-
 from time import sleep
 from datetime import datetime, timedelta
 def smart_sleep(nseconds):
@@ -32,3 +31,17 @@ def smart_sleep(nseconds):
     terminal_time = datetime.now() + timedelta(seconds=(nseconds+10))
     while(datetime.now() < terminal_time):
         sleep(120) # check every 2 minutes
+
+def do_shell(args): # convenience function to do run commandline stuff
+    """automatic retries"""
+    from subprocess import check_output,CalledProcessError
+    if isinstance(args, str):
+        args = args.split(" ")
+    n_failed = 0
+    while n_failed < 20:
+        try:
+            return check_output(args)
+        except CalledProcessError as e:
+            sleep(4)
+             n_failed +=1
+            print("Shell cmd failed with ",args)
