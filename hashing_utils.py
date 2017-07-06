969ac4816b0785b313a97dad357d975822a7a578
jerryxhe <rebound1618@gmail.com>
2017-07-06 10:27:25 -0400

    Create hashing_utils.py
    platform independent string hashing for python

---
+++ hashing_utils.py
@@ -0,0 +1,12 @@
+__author__ = "Jerry He"
+import zlib,sys
+
+def crc32_hash_py2(st):
+    return zlib.crc32(unicode(st)) & 0xffffffff
+    
+def crc32_hash_py3(st):
+    return zlib.crc32(str(st).encode("utf-8")) & 0xffffffff
+    
+crc32_hash = crc32_hash_py2 if sys.version[0]=='2' else crc32_hash_py3
+
+__all__ = ['crc_32_hash']
