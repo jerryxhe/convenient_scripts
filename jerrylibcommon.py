608a51df3784dc03bf24427329de6464fe9a2cac
jerryxhe <rebound1618@gmail.com>
2017-07-07 09:57:49 -0400

    Update jerrylibcommon.py
    get_py_version

--- jerrylibcommon.py
+++ jerrylibcommon.py
@@ -1,5 +1,11 @@
 # a collection of useful functions
 
+def get_py_version(_version=None):
+    if _version is None:
+        import sys
+        _version=sys.version_info.major
+    return _version
+
 def first_of(iterator_, condition_):
     for item in iterator_:
         try:
