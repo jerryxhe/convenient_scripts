9878496bac7941637d6ce9753907564d36a092d5
jerryxhe <rebound1618@gmail.com>
2017-07-10 12:53:05 -0400

    Create ca.groovy
    android build automation

---
+++ ca.groovy
@@ -0,0 +1,6 @@
+!/Users/jerry/.sdkman/candidates/groovy/current/bin/groovy
+
+"ionic platform remove android".execute()
+"ionic platform android".execute()
+"cp *.lua platforms/android/assets".execute()
+"ionic run android".execute()
