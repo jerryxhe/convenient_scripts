96f27a0503467493484584430616b67ac7b2ba73
jerryxhe <rebound1618@gmail.com>
2017-07-12 16:01:46 -0400

    Update scrape_utils.py

--- scrape_utils.py
+++ scrape_utils.py
@@ -6,3 +6,11 @@ def html_remove(text):
         if ent in text:
             text=text.replace('&'+ent+';', repla)
     return text.replace('&#39;', "'")
+
+import string
+def remove_punct(word, _punkts=string.punctuation):
+    return "".join(c for c in word if c not in _punkts)
+
+from bs4 import BeautifulSoup
+def remove_html(_text):
+    return BeautifulSoup(_text, "lxml").text
