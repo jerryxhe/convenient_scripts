271637ba47bb82a9b9745e8bf0db47fa9b0c6308
jerryxhe <rebound1618@gmail.com>
2017-07-13 14:24:17 -0400

    Update compat_utils.py

--- compat_utils.py
+++ compat_utils.py
@@ -1,3 +1,11 @@
+import sys
+
+def get_py_version(_version=None):
+	if _version is None:
+		#import sys
+		_version = sys.version_info.major
+	return _version
+
 def first_exist(*args):
     for o in args:
         try:
