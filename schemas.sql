84566c19fa832c7703b1607b36981660ca574444
jerryxhe <rebound1618@gmail.com>
2017-07-24 23:02:51 -0400

    Update schemas.sql
    adding wsj pdf storage schema

--- schemas.sql
+++ schemas.sql
@@ -1,3 +1,12 @@
+CREATE TABLE wsj_gdrive (
+issue DATE NOT NULL,
+section varchar(4) NOT NULL,
+pdf_hash VARCHAR(1023) NOT NULL,
+gdrive_id VARCHAR(1023) NOT NULL,
+ne varchar(127)[],
+metadata jsonb
+);
+
 CREATE TABLE sentence_translations ( 
 article_id varchar(11) NOT NULL,     
 sentence varchar(2047) NOT NULL, 
